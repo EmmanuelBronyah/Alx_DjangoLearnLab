ee17a170b43bc738a2e997069981bf9c72215e26
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2024-08-13 20:19:56 +0000

    Made modifications.

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -23,11 +23,11 @@ class Library(models.Model):
     return self.name
 
 
-class Librarian(models.Model):
-  name = models.CharField(max_length=200)
-  library = models.OneToOneField(Library, on_delete=models.CASCADE)
+# class Librarian(models.Model):
+#   name = models.CharField(max_length=200)
+#   library = models.OneToOneField(Library, on_delete=models.CASCADE)
   
-  def __str__(self):
-    return self.name
+#   def __str__(self):
+#     return self.name
 
 
