dca2c5f1b86092e05dae15adff5fac4b905df239
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2024-08-13 21:55:53 +0000

    Made modifications.

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Library
 from .models import Library
-from django.views.generic import DetailView, ListView
+from django.views.generic.detail import DetailView
 
 
 def list_books(request):
