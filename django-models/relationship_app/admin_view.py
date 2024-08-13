d8050f383ddcea2b2a78b1060b7a3d0e920733e9
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2024-08-13 23:47:58 +0000

    Made modifications.

--- django-models/relationship_app/admin_view.py
+++ django-models/relationship_app/admin_view.py
@@ -1,9 +1,12 @@
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import TemplateView
 
+class AdminView(UserPassesTestMixin, TemplateView):
+    template_name = 'admin_view.html'
 
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    def test_func(self):
+        return self.request.user.is_authenticated and self.request.user.userprofile.role == 'Admin'
 
-@user_passes_test(is_admin)
-def Admin(request):
-    return render(request, 'admin_view.html')
+    def handle_no_permission(self):
+        # Optional: Redirect to a custom page or show a message if the user does not have permission
+        return redirect('login')
