f01da3ecf5dc9c4514a40e030c6cafa0d6476a41
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2024-08-13 23:02:05 +0000

    Made modifications.

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,7 +7,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path("list_books/", list_books, name="list-books"),
     path("library_detail/", views.LibraryDetailView.as_view(), name="library-detail"),
-    path("register/", views.RegisterView.as_view(), name="register"),
-    path('login/', views.RegisterView.as_view(), name="login"),
-    path('logout/', views.RegisterView.as_view(), name="logout"),
+    path("register/", views.register, name="register"),
+    path('login/', views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path('logout/', views.LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -26,6 +26,9 @@ class LibraryDetailView(ListView):
         return context
 
 
+def register(request):
+  pass
+
 class LoginView(LoginView):
     template_name = 'login.html'
 
