80009e071eb38639b3b0b4cd8c73331a17e45d0b
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2024-08-28 13:51:16 +0000

    Finished permissions practice.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, permissions
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework.permissions import BasePermission
 
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        return request.user and request.user.is_staff
 
 
 class BookList(generics.ListAPIView):
@@ -12,3 +19,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
+  permission_classes = [permissions.IsAuthenticated, IsAdminOrReadOnly]
