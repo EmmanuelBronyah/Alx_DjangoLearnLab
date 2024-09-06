6f3796e7e493de3e169233dc4f0d792113a44284
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2024-09-06 09:32:32 +0000

    Made modifications

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
-# Create your views here.
+
+class BookListView(generics.ListCreateAPIView):
+    """
+    Handles listing all books and creating a new book.
+    Only authenticated users can create a new book.
+    """
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Handles retrieving, updating, and deleting a specific book by its ID.
+    Update and delete actions are restricted to authenticated users.
+    """
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
