59aceb92b4d64a61a9801b62afd0fd95bde284c7
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2024-09-20 23:26:34 +0000

    Made modifications

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, UserFeedViewSet
 
+# Initialize the router
 router = DefaultRouter()
+
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 router.register(r'feed', UserFeedViewSet, basename='user-feed')
